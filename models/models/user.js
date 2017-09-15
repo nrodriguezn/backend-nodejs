028d8d742b74d2de84758fa435e77e562ed63e72
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-09-15 16:24:01 -0500

    user.js, schema para usuario

---
+++ models/models/user.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')//para hashear y encriptar contraseñas
+const crypto = require('crypto')
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  displayName: String,
+  avatar: String,
+  password: { type: String, select: false }, //para al hacer un select no entrega la contraseña
+  signupDate: { type: Date, default: Date.now() },
+  lastLogin: Date
+})
+
+//se puede ejecutar antes o despues de que el modelo haya sido guardado, en este caso antes
+UserSchema.pre('save', (next) => {
+  let user = this
+  if (!user.isModified('password')) return next()
+
+  bcrypt.genSalt(10, (err, salt) = {
+    if (err) return next()
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err)
+      user.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.gravatar = function (){
+  if(!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
+
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+  return `https://gravatar.com/avatar/${mdg}?s=200&d=retro`
+}
+
+model.exports = mongoose.model('User', UserSchema)
